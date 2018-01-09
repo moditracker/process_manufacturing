5ed6a0235595033c8983e51533a03b3d066a5fb2
Asharam Seervi <seerviasharam@gmail.com>
2018-01-09 15:25:05 +0530

    Updated: Whitelabelled Oztro PT to Process Type

--- process_manufacturing/process_manufacturing/doctype/process_type/process_type.js
+++ process_manufacturing/process_manufacturing/doctype/process_type/process_type.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2017, earthians and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Oztro Process Type', {
+frappe.ui.form.on('Process Type', {
 	refresh: function(frm) {
 
 	}
--- process_manufacturing/process_manufacturing/doctype/process_type/process_type.py
+++ process_manufacturing/process_manufacturing/doctype/process_type/process_type.py
@@ -6,5 +6,5 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 
-class OztroProcessType(Document):
+class ProcessType(Document):
 	pass
